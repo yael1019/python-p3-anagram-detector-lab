a83a79224a4dc6a37870229d17c95d23203a3fb2
Yael Harosh <yaelharosh01@gmail.com>
2023-04-06 08:59:06 -0400

    pass tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, anagrams):
+        result = []
+        list_of_letters = list(self.word)
+        list_of_letters.sort()
+        # print(list_of_letters)
+        for word in anagrams:
+            anagram = list(word)
+            anagram.sort()
+            # print(anagram)
+            if list_of_letters == anagram:
+               result.append(word)
+        
+        return result
+
+
+
+# Anagram("word")
+# Anagram("olleh").match(["hello", "goodbye"])
